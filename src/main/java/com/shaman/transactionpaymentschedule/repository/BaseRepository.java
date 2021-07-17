cc69003dab88ded6cd555bdf24c88fb088e41137
josephdsbr@gmail.com <josephdsbr@gmail.com>
2021-07-17 17:59:06 -0300

    Ajustando o tipagens do BaseRepository

--- src/main/java/com/shaman/transactionpaymentschedule/repository/BaseRepository.java
+++ src/main/java/com/shaman/transactionpaymentschedule/repository/BaseRepository.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @NoRepositoryBean
 public interface BaseRepository<T> extends JpaRepository<T, Long> {
     List<T> findAllByRemovedAtIsNull();
-    T findByUuidAndRemovedAtIsNull();
-    T findByIdAndRemovedAtIsNull();
+    Optional<T> findByUuidAndRemovedAtIsNull(UUID uuid);
+    Optional<T> findByIdAndRemovedAtIsNull(Long id);
 }
